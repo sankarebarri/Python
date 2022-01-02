83b3dc5de2a2f7efae62c872a866c37726105df5
sankarebarri <sankarebarri_dev@yahoo.com>
2022-01-02 22:13:34 +0000

    Data Structures and Algorithms folder created

--- List/reverse_list.py
+++ List/reverse_list.py
@@ -6,3 +6,13 @@ def reverseList(array):
     accumulator = []
     for x in array:
         accumulator = [x] + accumulator
+    return accumulator
+
+def main():
+    if __name__=="__main__":
+        L = [1,2,3,4,5]
+        L1 = ['a','b','c','d','e']
+        print(reverseList(L))
+        print(reverseList(L1))
+
+main()
