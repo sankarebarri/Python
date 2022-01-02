881b9b3d4c5340431aba1f483984839970631ca6
sankarebarri <sankarebarri_dev@yahoo.com>
2022-01-02 22:41:44 +0000

    isPerfectCube

--- DataStructuresAndAlgorithm/GuessAndCheckAlgo.py
+++ DataStructuresAndAlgorithm/GuessAndCheckAlgo.py
@@ -1,1 +1,17 @@
-x = 36
+def isPerfectCube(num):
+    ans = 0
+    while ans**3 < abs(num):
+        ans += 1
+    if ans**3 != abs(num):
+        print("Not a perfect cube")
+    else:
+        if num < 0:
+            ans = -ans
+        print(ans, "is a cube root of", num)
+def main():
+    if __name__ == "__main__":
+        print(isPerfectCube(8), "answer is 2")
+        print(isPerfectCube(-8), "answer is -2")
+        print(isPerfectCube(9), "answer is not perfect cube")
+        
+main()
