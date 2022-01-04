343768da8284883b7067992914528b78f49a3e18
sankarebarri <sankarebarri_dev@yahoo.com>
2022-01-04 22:16:43 +0000

    consecutiveAlphabetCount

---
+++ Functions/consecutiveAlphabet.py
@@ -0,0 +1,12 @@
+
+def consecutiveAlphabet(s):
+    pass
+
+
+def main():
+    s = 'azcbobobegghakl'
+    s1 = 'ndgcfgfhabcdefg'
+    print(consecutiveAlphabet(s),'beggh')
+    print(consecutiveAlphabet(s1),'abcdefg')
+
+main()
