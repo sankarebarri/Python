af43ef31f16bea8b5b6570711ef407d1d03990d2
sankarebarri <sankarebarri_dev@yahoo.com>
2022-01-07 10:49:18 +0000

    filter.py

--- BuiltInFunctions/filter.py
+++ BuiltInFunctions/filter.py
@@ -1,7 +1,11 @@
 # filter out values that are less than the average
 import statistics
-data = [1.2, 5, 1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
+values = [1.2, 5, 1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
 
-avg = statistics.mean(data)
+avg = statistics.mean(values)
 
-print(list(filter(lambda x: x > avg, data)))
+print(list(filter(lambda x: x > avg, values)))
+
+# filter out empty data
+data = ['', '1', 5, 'hello', '5+6', '', '....']
+print(list(filter(None, data)))
