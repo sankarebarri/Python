60e95b5e5900c410d33818e052522e882752dcf7
sankarebarri <sankarebarri_dev@yahoo.com>
2022-02-01 23:09:44 +0000

    SalesPerson

---
+++ Classes/SalesPerson.py
@@ -0,0 +1,11 @@
+class SalesPerson:
+    def __int__(self, first_name, last_name, employee_id, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.employee_id = employee_id
+        self.salary = salary
+        self.pants_sold = []
+        self.total_sales = 0
+
+    def sell_pants(self):
+        pass
