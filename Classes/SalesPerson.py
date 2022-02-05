b230b2a925d477d3a3bbd6142951b24e6e73dab2
sankarebarri <sankarebarri_dev@yahoo.com>
2022-02-05 23:06:46 +0000

    Sales Person Class

--- Classes/SalesPerson.py
+++ Classes/SalesPerson.py
@@ -7,5 +7,19 @@ class SalesPerson:
         self.pants_sold = []
         self.total_sales = 0
 
-    def sell_pants(self):
-        pass
+    def sell_pants(self, pants_object):
+        self.pants_sold.append(pants_object)
+
+    def display_sales(self):
+        for pant in self.pants_sold:
+            print(f'color: {pant.color}, waist_size: {pant.waist_size}, length: {pant.length}, price:{pant.price}')
+
+    def calculate_sales(self):
+        total_sales = 0
+        for pant in self.pants_sold:
+            total_sales = total_sales + pant.price
+        return total_sales
+
+    def calculate_commision(self, percentage):
+        total_sales = self.calculate_sales()
+        return total_sales * percentage
