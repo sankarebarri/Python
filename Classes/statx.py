b37731e26ea958e53ccae5ce7e044462f431a599
sankarebarri <sankarebarri_dev@yahoo.com>
2022-02-10 22:42:43 +0000

    Statistics Class

---
+++ Classes/statx.py
@@ -0,0 +1,50 @@
+class Statx:
+    
+    def __init__(self):
+        self.data = []
+        pass
+    
+    def data_mean(self):
+        pass
+    
+    def data_variance(self):
+        pass
+    
+    def data_stdev(self):
+        pass
+    
+    def data_covariance(self):
+        pass
+    
+    def z_score(self):
+        pass
+    
+    def data_median(self):
+        pass
+    
+    def skewed(self):
+        '''Tells if a data is skewed
+        Returns:
+            Boolean: False if not skewed
+            If Skewed:
+                Return Skewed direction
+        '''
+        pass
+    
+    def is_sampling_normal(self):
+        '''
+        Tells if a sampling distribution is normal according to some given rules
+        '''
+        pass
+    
+    def sample_mean(self):
+        pass
+    
+    def sample_proportion(self):
+        pass
+    
+    def standard_error_mean(self):
+        pass
+    
+    def confidence_interval(self):
+        pass
