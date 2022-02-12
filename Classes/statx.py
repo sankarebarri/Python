2b46cef89ea2b7fea7e79f2d4beb777c45b3c88a
sankarebarri <sankarebarri_dev@yahoo.com>
2022-02-12 01:04:33 +0000

    data_median() method coded

--- Classes/statx.py
+++ Classes/statx.py
@@ -3,9 +3,35 @@ class Statx:
     def __init__(self):
         self.data = []
         pass
+
+    def read_data(self):
+        pass
     
     def data_mean(self):
-        pass
+        '''
+        Calculate the mean of:
+            the sample_data if sample=True
+            the population if sample =False
+            
+        Args:
+            sample: boolean
+        Return: The mean of the data
+        '''
+        len_data = len(self.data)
+        if sample:
+            len_data = len_data - 1
+            return sum(self.data)/len_data
+        return sum(self.data)/len_data
+
+    def data_median(self):
+        '''
+        Gives the median of the data
+        '''
+        self.data.sort()
+        length = len(self.data)
+        if length%2 !=0:
+            return self.data[length//2]
+        return((self.data[length//2] + (self.data[length//2] - 1)) / 2)
     
     def data_variance(self):
         pass
@@ -17,9 +43,6 @@ class Statx:
         pass
     
     def z_score(self):
-        pass
-    
-    def data_median(self):
         pass
     
     def skewed(self):
